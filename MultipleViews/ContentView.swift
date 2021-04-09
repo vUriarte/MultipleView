0e721290ede97c1c3eda3eb95607b91cb2b84314
uriarte <uriarte0505@gmail.com>
2021-04-09 17:47:13 -0300

    Creating a NavigationVier List based on DataModel

--- MultipleViews/ContentView.swift
+++ MultipleViews/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(DataModel.data, id: \.self) {object in
+                Text(object.title)
+            }
+            .navigationTitle("NavigationList")
+        }
     }
 }
 
