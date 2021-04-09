779d8cbc0b63d549d56ab853caa71fbea1161e2b
uriarte <uriarte0505@gmail.com>
2021-04-09 17:25:45 -0300

    Initial Commit

---
+++ MultipleViews/MultipleViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleViewsApp.swift
+//  MultipleViews
+//
+//  Created by Victor Uriarte on 09/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MultipleViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
